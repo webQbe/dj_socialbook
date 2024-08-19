996b6c5ee399be854f93a255dd1bfa6367394cdf
webQbe <webcubesolutionslk@gmail.com>
2024-08-19 10:59:24 +0530

    template setup completed

--- core/views.py
+++ core/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Welcome To Social Book</h1>')
+    return render(request, 'index.html')
 
 
