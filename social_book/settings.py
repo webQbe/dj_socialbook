69b377688f6454a50093f28b3b74b0607f32b631
webQbe <webcubesolutionslk@gmail.com>
2024-08-21 12:34:38 +0530

    user search enabled

--- social_book/settings.py
+++ social_book/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'social_book.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / 'templates'], #[os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
